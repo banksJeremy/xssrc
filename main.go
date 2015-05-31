a940ef744f791c73bee2c05c8674a95710b353ac
Jeremy Banks <j@jeremybanks.ca>
2015-05-30 22:50:19 -0700

    m

--- main.go
+++ main.go
@@ -1,4 +1,3 @@
-///bin/true; exec /usr/bin/env go run "$0" "$@"
 package main
 
 import (
@@ -9,13 +8,13 @@ import (
 )
 
 type CircuitConnection struct {
-    Ws *ws.Conn
+	Ws *websocket.Conn
 }
 
 type PendingRequest struct {
-  	Request *http.Request
-  	// the CircuitConnection that is currently handling this reuest, or nil
-  	HandlingConnection *CircuitConnection
+	Request *http.Request
+	// the CircuitConnection that is currently handling this reuest, or nil
+	HandlingConnection *CircuitConnection
 }
 
 type Circuit struct {
@@ -26,19 +25,20 @@ type Circuit struct {
 }
 
 func circuitHandler(ws *websocket.Conn) {
-	fmt.Println("Got connection ", ws)
+	fmt.Println("Got WS connection ", ws)
+}
+
+func placeholderRedirectHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Got HTTP request", w, r)
 }
 
 func main() {
 	http.Handle("/__xssrc__/circut-connection", websocket.Handler(circuitHandler))
-	http.Handle("/__xssrc__", http.FileServer(http.Dir("static")))
+	http.Handle("/__xssrc__/", http.FileServer(http.Dir("static")))
+	http.Handle("/", http.HandlerFunc(placeholderRedirectHandler))
 
-	fmt.Println(&Circuit{})
-
-	fmt.Println("WebSockets Version", websocket.SupportedProtocolVersion)
-
-	// Intentionally loopback-only. Do not change this to 0.0.0.0.
-	err := http.ListenAndServe("127.0.0.1:8064", nil)
+	fmt.Println("Starting HTTP server")
+	err := http.ListenAndServe("0.0.0.0:8080", nil)
 	if err != nil {
 		panic("Server error: " + err.Error())
 	}
@@ -46,14 +46,14 @@ func main() {
 
 /*
 TO DO
--  
+-
 - After getting a request, wait some amount of time for a Circuit to
    become available to handle it, then send a 504 Gateway Timeout.
  - Always send 504 Gateway Timeout with a Refresh header
  - Support GET and POST requests, and arbitrary responses
  - Use an XSSRC- header to indicate requests that are not proxied.
  - Add very visible console logging on circuit clients, including the IPs of connecting
-   controllers. Maybe persist some information (in a compact form) in localStorage, to enable a sort-of auditing.
+   browsers. Maybe persist some information (in a compact form) in localStorage, to enable a sort-of auditing.
  - Alway use the older circuit that is stil responsive, to avoid uses from switching between sessions (though
    that won't happen in ideal circuimstances anyway).
 */
